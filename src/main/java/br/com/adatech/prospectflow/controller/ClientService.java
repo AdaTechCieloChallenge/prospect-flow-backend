10af714d5359b34296a7be43736a842016e4ae22
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2023-09-26 15:11:44 -0300

    Feat: Initial solution modeling for backend with queue service, validation and persistence layers ready to be tested.

--- src/main/java/br/com/adatech/prospectflow/controller/ClientService.java
+++ src/main/java/br/com/adatech/prospectflow/controller/ClientService.java
@@ -10,7 +10,6 @@ import br.com.adatech.prospectflow.infra.database.ClientPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Optional;
 
