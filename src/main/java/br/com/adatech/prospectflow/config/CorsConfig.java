d249d363ccc21973ad37738c45fdc5a317c71420
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2023-09-28 17:29:05 -0300

    Feat: Forcing CORS configuration to futher integration tests and add swagger docs.

---
+++ src/main/java/br/com/adatech/prospectflow/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package br.com.adatech.prospectflow.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//Forçando a habilitação apenas para testes de integração. Lembrar de restringir escopo.
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("*");
+    }
+}
